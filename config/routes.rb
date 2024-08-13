8a124b65e7e40c9a6e0f9939c65759c3970b1883
Carmine Paolino <carmine@paolino.me>
2024-08-13 09:51:20 +0200

    index route

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "headache_logs#index"
 end
