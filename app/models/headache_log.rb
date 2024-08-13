75888c78b38d5f25c744abe03a56edbea3d0e78b
Carmine Paolino <carmine@paolino.me>
2024-08-13 09:52:00 +0200

    linting and changed default mail sender

--- app/models/headache_log.rb
+++ app/models/headache_log.rb
@@ -3,5 +3,5 @@ class HeadacheLog < ApplicationRecord
   validates :start_time, :intensity, presence: true
   validates :intensity, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 
-  broadcasts_to ->(headache_log) { [headache_log.user, "headache_logs"] }, inserts_by: :prepend
+  broadcasts_to ->(headache_log) { [ headache_log.user, "headache_logs" ] }, inserts_by: :prepend
 end
