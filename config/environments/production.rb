d07cdd776173bfe6c21892c694d5b84b3be7f0e5
Carmine Paolino <carmine@paolino.me>
2024-08-14 11:18:17 +0200

    fixing render serving js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
