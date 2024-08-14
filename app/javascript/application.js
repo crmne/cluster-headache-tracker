ac098ed5069441481322ce4be8c85dd771918af9
Carmine Paolino <carmine@paolino.me>
2024-08-14 13:36:27 +0200

    trying to fix the chart bug on render

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import { initializeCharts } from "./charts"
+import { initializeCharts } from "./charts.js"
 
 window.initializeCharts = initializeCharts;
