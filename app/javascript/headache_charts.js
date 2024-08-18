0aa88186f95e886d660db7967d286921b2ce650a
Carmine Paolino <carmine@paolino.me>
2024-08-18 12:03:20 +0200

    fix #5: canvas already in use

--- app/javascript/headache_charts.js
+++ app/javascript/headache_charts.js
@@ -26,41 +26,53 @@ Chart.register(
   Tooltip
 );
 
-export function initializeCharts(intensityData, triggerData, medicationData) {
-  const intensityCtx = document.getElementById('intensityChart');
-  if (intensityCtx) {
-    new Chart(intensityCtx, {
-      type: 'line',
-      data: {
-        datasets: [{
-          label: 'Headache Intensity',
-          data: intensityData,
-          borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1
-        }]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          x: {
-            type: 'time',
-            time: {
-              unit: 'day'
+let chartInstances = {};
+
+function initializeIntensityChart(intensityData) {
+  if (intensityData && Object.keys(intensityData).length > 0) {
+    const intensityCtx = document.getElementById('intensityChart');
+    if (intensityCtx) {
+      if (chartInstances.intensityChart) {
+        chartInstances.intensityChart.destroy();
+      }
+      chartInstances.intensityChart = new Chart(intensityCtx, {
+        type: 'line',
+        data: {
+          datasets: [{
+            label: 'Headache Intensity',
+            data: intensityData,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          }]
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              type: 'time',
+              time: {
+                unit: 'day'
+              }
+            },
+            y: {
+              beginAtZero: true,
+              max: 10
             }
-          },
-          y: {
-            beginAtZero: true,
-            max: 10
           }
         }
-      }
-    });
+      });
+    }
   }
+}
 
+function initializeTriggerChart(triggerData) {
   if (triggerData && Object.keys(triggerData).length > 0) {
     const triggerCtx = document.getElementById('triggerChart');
     if (triggerCtx) {
-      new Chart(triggerCtx, {
+      if (chartInstances.triggerChart) {
+        chartInstances.triggerChart.destroy();
+      }
+      chartInstances.triggerChart = new Chart(triggerCtx, {
         type: 'pie',
         data: {
           labels: Object.keys(triggerData),
@@ -90,10 +102,16 @@ export function initializeCharts(intensityData, triggerData, medicationData) {
       });
     }
   }
+}
+
+function initializeMedicationChart(medicationData) {
   if (medicationData && Object.keys(medicationData).length > 0) {
     const medicationCtx = document.getElementById('medicationChart');
     if (medicationCtx) {
-      new Chart(medicationCtx, {
+      if (chartInstances.medicationChart) {
+        chartInstances.medicationChart.destroy();
+      }
+      chartInstances.medicationChart = new Chart(medicationCtx, {
         type: 'pie',
         data: {
           labels: Object.keys(medicationData),
@@ -123,4 +141,10 @@ export function initializeCharts(intensityData, triggerData, medicationData) {
       });
     }
   }
+}
+
+export function initializeCharts(intensityData, triggerData, medicationData) {
+  initializeIntensityChart(intensityData);
+  initializeTriggerChart(triggerData);
+  initializeMedicationChart(medicationData);
 }
