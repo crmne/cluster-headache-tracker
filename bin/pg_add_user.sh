3d5d9b97cee273188e2dc567e7f8e4a7af8498c6
Carmine Paolino <carmine@paolino.me>
2024-10-08 11:40:13 +0200

    Readding bin (2/2)

---
+++ bin/pg_add_user.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# exit on error
+set -o errexit
+
+# Check if a username was provided
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <username>"
+  exit 1
+fi
+
+# Get the parameters from the arguments
+USERNAME="$1"
+
+# Read password from stdin
+read -s -p "Creating user $USERNAME. Enter password: " -r PASSWORD
+
+# Create the SQL command
+SQL="CREATE USER $USERNAME WITH PASSWORD '$PASSWORD' CREATEDB WITH SUPERUSER;"
+
+# Execute the command
+DBNAME="postgres"
+psql -d "$DBNAME" -c "$SQL" ||
+  sudo -u postgres psql -d "$DBNAME" -c "$SQL"
+
+echo "User $USERNAME has been created."
