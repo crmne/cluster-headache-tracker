97c31d7fbfc635d6c7daa77f09396b616b67123b
Carmine Paolino <carmine@paolino.me>
2024-10-18 14:41:35 +0200

    Import hotwire-native

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,9 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@hotwired/hotwire-native-bridge", to: "@hotwired--hotwire-native-bridge.js" # @1.0.0
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Chart.js and its dependencies
