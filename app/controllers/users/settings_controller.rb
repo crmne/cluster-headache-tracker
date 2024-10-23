73784ceaffa322e30ccae292a1ea3681e85abbd2
Carmine Paolino <carmine@paolino.me>
2024-10-23 18:08:59 +0200

    WIP Account settings and Feedback

---
+++ app/controllers/users/settings_controller.rb
@@ -0,0 +1,39 @@
+class Users::SettingsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def show
+  end
+
+  def update_username
+    if @user.update_with_password(username_params)
+      bypass_sign_in(@user)
+      redirect_to settings_path, notice: "Username was successfully updated."
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  def update_password
+    if @user.update_with_password(password_params)
+      bypass_sign_in(@user)
+      redirect_to settings_path, notice: "Password was successfully updated."
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def username_params
+    params.require(:user).permit(:username, :current_password)
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation, :current_password)
+  end
+end
