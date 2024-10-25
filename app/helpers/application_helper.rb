f985b3fdfdca86f93dd3da1d0e767c0fb9deb831
Carmine Paolino <carmine@paolino.me>
2024-10-25 17:07:09 +0200

    WIP time_ago_in_words(format: :short)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def time_ago_in_words(from_time, format: :long)
+    return unless from_time
+
+    distance = (Time.current - from_time).round
+
+    case format
+    when :short
+      if distance < 1.hour
+        pluralize((distance / 60).round, "min")
+      elsif distance < 24.hours
+        pluralize((distance / 1.hour).round, "hour")
+      else
+        pluralize((distance / 1.day).round, "day")
+      end
+    else
+      super(from_time)
+    end
+  end
 end
