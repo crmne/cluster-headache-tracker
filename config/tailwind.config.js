b58fab2796fad3e6fc221fe51e9c45a80ee5c483
Carmine Paolino <carmine@paolino.me>
2024-11-14 11:44:26 +0100

    WIP print layout for shared_logs

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      screens: {
+        'print': {'raw': 'print'}
+      }
     },
   },
   darkMode: 'media',
