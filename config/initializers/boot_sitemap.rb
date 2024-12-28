703f5c6bb1fe6bb26d100e7f094a76ff1d1a1f4c
Carmine Paolino <carmine@paolino.me>
2024-12-28 10:55:22 +0100

    Refresh sitemap after a minute

--- config/initializers/boot_sitemap.rb
+++ config/initializers/boot_sitemap.rb
@@ -1,6 +1,6 @@
 Rails.application.config.after_initialize do
   if defined?(Rails::Server)
-    # Give the app a few seconds to fully initialize before generating sitemap
-    SitemapRefreshJob.set(wait: 10.seconds).perform_later
+    # Give the app a minute to fully initialize before generating sitemap
+    SitemapRefreshJob.set(wait: 1.minute).perform_later
   end
 end
