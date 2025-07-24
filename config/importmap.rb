fa5e1903bcbcf77311737682ff2e3b436946a404
Carmine Paolino <carmine@paolino.me>
2025-07-24 10:28:26 +0200

    Simplify importmap and fix babel-runtime vulnerability
    - Remove individual babel-runtime pins
    - Switch to bundled UMD builds from CDN
    - Remove unnecessary dependencies (@kurkle/color, date-fns)
    - Clean up unused tailwind.config pin
    
    This fixes the security vulnerability in babel-runtime < 7.26.10
    and follows Rails conventions for minimal dependencies.

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,29 +1,14 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "tailwind.config"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# Chart.js and its dependencies
-pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.3.0/dist/chart.js"
-pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
-pin "chartjs-adapter-date-fns", to: "https://ga.jspm.io/npm:chartjs-adapter-date-fns@3.0.0/dist/chartjs-adapter-date-fns.esm.js"
-pin "date-fns", to: "https://ga.jspm.io/npm:date-fns@2.30.0/esm/index.js"
-
-# Add these new pins for @babel/runtime
-pin "@babel/runtime/helpers/esm/typeof", to: "https://ga.jspm.io/npm:@babel/runtime@7.22.5/helpers/esm/typeof.js"
-pin "@babel/runtime/helpers/esm/toPrimitive", to: "https://ga.jspm.io/npm:@babel/runtime@7.22.5/helpers/esm/toPrimitive.js"
-pin "@babel/runtime/helpers/esm/toPropertyKey", to: "https://ga.jspm.io/npm:@babel/runtime@7.22.5/helpers/esm/toPropertyKey.js"
-pin "@babel/runtime/helpers/esm/createForOfIteratorHelper", to: "https://ga.jspm.io/npm:@babel/runtime@7.22.5/helpers/esm/createForOfIteratorHelper.js"
-pin "@babel/runtime/helpers/esm/assertThisInitialized", to: "https://ga.jspm.io/npm:@babel/runtime@7.22.5/helpers/esm/assertThisInitialized.js"
-pin "@babel/runtime/helpers/esm/inherits", to: "https://ga.jspm.io/npm:@babel/runtime@7.22.5/helpers/esm/inherits.js"
-pin "@babel/runtime/helpers/esm/createSuper", to: "https://ga.jspm.io/npm:@babel/runtime@7.22.5/helpers/esm/createSuper.js"
-pin "@babel/runtime/helpers/esm/classCallCheck", to: "https://ga.jspm.io/npm:@babel/runtime@7.22.5/helpers/esm/classCallCheck.js"
-pin "@babel/runtime/helpers/esm/createClass", to: "https://ga.jspm.io/npm:@babel/runtime@7.22.5/helpers/esm/createClass.js"
-pin "@babel/runtime/helpers/esm/defineProperty", to: "https://ga.jspm.io/npm:@babel/runtime@7.22.5/helpers/esm/defineProperty.js"
+# Chart.js with date adapter
+pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.8/dist/chart.umd.js"
+pin "chartjs-adapter-date-fns", to: "https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns@3.0.0/dist/chartjs-adapter-date-fns.bundle.min.js"
 
 # Charts
 pin "headache_charts", to: "headache_charts.js"
