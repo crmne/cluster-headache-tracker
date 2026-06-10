87d9dce381ab35b6a753f174f35264454369d685
Carmine Paolino <carmine@paolino.me>
2026-06-10 09:57:43 +0200

    Remove uncalled ShareToken#active? duplicate of active scope

--- app/models/share_token.rb
+++ app/models/share_token.rb
@@ -5,8 +5,4 @@ class ShareToken < ApplicationRecord
   before_create { self.expires_at = 30.days.from_now }
 
   scope :active, -> { where("expires_at > ?", Time.current) }
-
-  def active?
-    expires_at > Time.current
-  end
 end
